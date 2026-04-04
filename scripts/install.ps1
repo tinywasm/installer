2579e08e71f9702ad4c3b325bce61ce05c82e0dc
Cesar Solis <44058491+cdvelop@users.noreply.github.com>
2026-04-04 01:17:33 -0300

    fix: update install.ps1 to installer v0.0.27

--- scripts/install.ps1
+++ scripts/install.ps1
@@ -27,6 +27,6 @@ $env:PATH = "$goBin;$userGoBin;$machinePath"
 go version
 
 # 6. Install and run the Go orchestrator
-go install github.com/tinywasm/installer/cmd/installer@v0.0.25
+go install github.com/tinywasm/installer/cmd/installer@v0.0.27
 $installerBin = "$env:USERPROFILE\go\bin\installer.exe"
 & $installerBin $toolsFlag
